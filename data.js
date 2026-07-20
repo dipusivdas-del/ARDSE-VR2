176b9eb8d10ed531bc4d8523c0bd5a6bf68adbdc
dipusivdas-del <dipusivdas@gmail.com>
2026-07-20 07:57:24 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,350 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-family-living-cum-dining-",
+      "name": "FAMILY LIVING CUM DINING ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.0120184004921544,
+        "pitch": -0.054277629054192644,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.408301318015827,
+          "pitch": 0.26098020427407675,
+          "rotation": 0,
+          "target": "1-gf--bed"
+        },
+        {
+          "yaw": -2.7108959967082136,
+          "pitch": 0.21029726416524142,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -0.25402917952768256,
+          "pitch": 0.2801776157152158,
+          "rotation": 1.5707963267948966,
+          "target": "4-ff-stair-inside"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gf--bed",
+      "name": "GF  BED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1844052603318937,
+          "pitch": 0.014478244513949079,
+          "rotation": 1.5707963267948966,
+          "target": "3-work-area"
+        },
+        {
+          "yaw": 0.43778878078238037,
+          "pitch": 0.09060653761531157,
+          "rotation": 0,
+          "target": "0-family-living-cum-dining-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-work-area",
+      "name": "WORK AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7142713609976585,
+          "pitch": 0.2306047917181111,
+          "rotation": 0,
+          "target": "2-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ff-stair-inside",
+      "name": "FF STAIR INSIDE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5364889660729535,
+          "pitch": 0.5847557322202821,
+          "rotation": 4.71238898038469,
+          "target": "0-family-living-cum-dining-"
+        },
+        {
+          "yaw": 3.117901037209462,
+          "pitch": -0.2528540194276623,
+          "rotation": 1.5707963267948966,
+          "target": "5-first-floor-stair-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-first-floor-stair-area",
+      "name": "FIRST FLOOR STAIR AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43613341587342447,
+          "pitch": 0.14218852696905593,
+          "rotation": 3.141592653589793,
+          "target": "4-ff-stair-inside"
+        },
+        {
+          "yaw": 0.8693799808091534,
+          "pitch": 0.18656052088524433,
+          "rotation": 4.71238898038469,
+          "target": "7-first-floor-living-area"
+        },
+        {
+          "yaw": -1.644867527239029,
+          "pitch": 0.1662688848631948,
+          "rotation": 0,
+          "target": "6-ff-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ff-bedroom",
+      "name": "FF BEDROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9641865562407341,
+          "pitch": 0.08131098525693403,
+          "rotation": 0,
+          "target": "5-first-floor-stair-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-first-floor-living-area",
+      "name": "FIRST FLOOR LIVING AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2150521700421884,
+          "pitch": 0.43722968460877,
+          "rotation": 0,
+          "target": "5-first-floor-stair-area"
+        },
+        {
+          "yaw": -2.1095802423819308,
+          "pitch": 0.0350945729120582,
+          "rotation": 0,
+          "target": "6-ff-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ARDSE VR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
